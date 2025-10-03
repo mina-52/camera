37695b196f8d61995c58cb7ef5a03fd03ee0bfb7
yuya <izm70008@kwansei.ac.jp>
2025-10-03 11:47:20 +0900

    保存場所の変更

--- randoruto-test.py
+++ randoruto-test.py
@@ -518,8 +518,8 @@ if not cap.isOpened():
     print("OBSで「仮想カメラを開始」しているか、正しいカメラインデックスか確認してください。")
     exit()
 
-# randorutoフォルダーの作成
-save_folder = "randoruto"
+# randoruto-testフォルダーの作成
+save_folder = "randoruto-test"
 if not os.path.exists(save_folder):
     os.makedirs(save_folder)
     print(f"フォルダーを作成しました: {save_folder}")
